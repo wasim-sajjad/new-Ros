ad3346de3700fa47b6e47242947e65b0e66a3e7f
wasim-sajjad <wasim.sajjad@purelogics.net>
2018-09-14 20:49:07 +0500

    message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Login/login/login.component';
+import { ForgetPasswordComponent } from './Login/forget-password/forget-password.component';
+import { RegisterComponent } from './Login/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ForgetPasswordComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
